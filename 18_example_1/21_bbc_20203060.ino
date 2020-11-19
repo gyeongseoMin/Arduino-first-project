bb8cbb1bdd70006384d0e8780b22fce1d0a9045c
gyeongseoMin <min010724@naver.com>
2020-11-20 01:11:59 +0900

    second

---
+++ 18_example_1/21_bbc_20203060.ino
@@ -0,0 +1,55 @@
+// Arduino pin assignment
+#include <Servo.h>
+
+#define PIN_IR A0
+#define PIN_LED 9
+#define PIN_SERVO 10
+#define _DIST_ALPHA 0.1
+
+//global variables
+float dist_ema, alpha;
+
+Servo myservo;
+
+int a, b; // unit: mm
+void setup() {
+// initialize GPIO pins
+  pinMode(PIN_LED,OUTPUT);
+  digitalWrite(PIN_LED, 1);
+  myservo.attach(PIN_SERVO);
+  myservo.writeMicroseconds(1500);
+  delay(1000);
+// initialize serial port
+  Serial.begin(57600);
+  a = 70;
+  b = 300;
+  //
+  alpha = _DIST_ALPHA;
+}
+float ir_distance(void){ // return value unit: mm
+  float val;
+  float volt = float(analogRead(PIN_IR));
+  val = ((6762.0/(volt-9.0))-4.0) * 10.0;
+  return val;
+}
+void loop() {
+  
+  float raw_dist = ir_distance();
+  float dist_cali = 100 + 300.0 / (b - a) * (raw_dist - a);
+  dist_ema = raw_dist * alpha + (1-alpha) * dist_ema;
+  Serial.print("min:0,max:500,dist:");
+  Serial.print(raw_dist);
+  Serial.print(",dist_cali:");
+  Serial.println(dist_cali);
+  if(raw_dist < 225) {
+    digitalWrite(PIN_LED, 0);
+    myservo.writeMicroseconds(1800);
+    delay(100);
+  }
+  else {
+    digitalWrite(PIN_LED, 255);
+    myservo.writeMicroseconds(1350);
+    delay(100);
+  }
+  delay(20);
+}
